dfd430c24b2a4f490e7f0b691abd8b7e86bda14c
Anushka2966 <anushka.s.sidhartha@gmail.com>
2025-03-04 18:34:56 -0500

    fixed the .git issue

--- 2D_game_mechanics_1/godot-2d-space-game/project/ui/minimap/map_icon.gd
+++
@@ -1,7 +0,0 @@
-# Holds properties to draw icon representations on the mini map
-class_name MapIcon
-extends Resource
-
-@export var texture: Texture2D
-@export var color := Color.WHITE
-@export var scale := 1.0
